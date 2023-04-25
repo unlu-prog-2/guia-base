ad6e3179400e8d98d0521e59262d3f015d1a374e
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2023-04-25 15:39:51 -0300

    Práctica de pilas

---
+++ practicas/3-pilas/practica_pilas.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+
+#include "practica_pilas.h"
+#include "tipo_elemento.h"
+#include "pilas/pilas.h"
+
+
+bool buscarElemento(Pila p, TipoElemento e) {
+}
+
+void insertarElemento(Pila p, TipoElemento e, int pos) {
+}
+
+void eliminarElemento(Pila p, int clave) {
+}
+
+void intercambiarElementos(Pila p, int pos1, int pos2) {
+}
+
+Pila copiar(Pila p) {
+}
+
+int contarElementos(Pila p) {
+}
+
+bool compararPilas(Pila p1, Pila p2) {
+}
+
+char* cambioDeBase(int numero, int base) {
+}
+
+Pila invertirPila(Pila p) {
+}
+
+Pila eliminarTodasOcurrencias(Pila p, int clave) {
+}
+
+Pila elementosEnComun(Pila p1, Pila p2) {
+}
+
+Pila eliminarRepetidos(Pila p) {
+}
